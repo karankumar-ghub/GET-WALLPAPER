f228f0b1800c07b828f421fe122030da35c9d81a
Karan Rajput <136422563+karankumar-ghub@users.noreply.github.com>
2024-11-22 07:26:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import RandomAnimeAvatar from './Components/avatar'
+import RandomAnimeAvatar from './Components/Avatar'
 
 // import Card from './Components/Card'
 // import Cards from './Components/Cards'
